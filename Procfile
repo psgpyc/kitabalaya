a109efb38206a00ee716c7b6bb000075296a63ad
paritosh666 <paritosh.ghimire666@gmail.com>
2020-03-06 01:40:46 +0545

    added django-heroku package

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn kitabalaya.wsgi
-web: gunicorn kitabalaya.wsgi
--- kitabalaya/settings.py
+++ kitabalaya/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
@@ -131,3 +132,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
