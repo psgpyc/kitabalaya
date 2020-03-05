e77707141c80ae5ae7296ea71239e35ee660bad3
paritosh666 <paritosh.ghimire666@gmail.com>
2020-03-05 23:45:03 +0545

    Updated staticfiles location in respect with heroku documentation

--- kitabalaya/settings.py
+++ kitabalaya/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static-global'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
