e0904cb7d0757ce4de8734f1082cbe12095e403c
paritosh666 <paritosh.ghimire666@gmail.com>
2020-07-27 13:46:00 +0545

    updated settings file to match the recent change, removal of marathon app

--- kitabalaya/settings.py
+++ kitabalaya/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'comingsoon.apps.ComingsoonConfig',
     'corebookmodels.apps.CorebookmodelsConfig',
     'coreaccounts.apps.CoreaccountsConfig',
-    'marathon.apps.MarathonConfig',
+    # 'marathon.apps.MarathonConfig',
 
     # Third Party Applications
     'django_hosts',
